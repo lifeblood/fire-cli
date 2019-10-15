7754a7e24c16253ec8c3884768035f5bc0527fe5
danny <danny@wgame200.com>
2019-10-15 20:52:01 +0800

    first push

--- controllers/template.py
+++ controllers/template.py
@@ -1,17 +1,17 @@
 class Template(object):
-	def __init__(self, code=1):
-		self._code = code
+    def __init__(self, code=1):
+        self._code = code
 
-	@staticmethod
-	def run():
-		return 'Ingesting! Nom nom nom...'
+    @staticmethod
+    def run():
+        return 'Ingesting! Nom nom nom...'
 
-	def public_method(self):
-		print(self._code)
+    def public_method(self):
+        print(self._code)
 
-	def __private_method(self):
-		print(self._code)
+    def __private_method(self):
+        print(self._code)
 
-	@classmethod
-	def class_method(cls):
-		print(cls.run())
+    @classmethod
+    def class_method(cls):
+        print(cls.run())
--- controllers/test.py
+++ controllers/test.py
@@ -2,12 +2,12 @@
 
 
 class Test(object):
-	def __init__(self, code=1):
-		self._code = code
+    def __init__(self, code=1):
+        self._code = code
 
-	def hello(self):
-		print(self._code)
+    def hello(self):
+        print(self._code)
 
-	@staticmethod
-	def run():
-		return 'Ingesting! Nom nom nom...'
+    @staticmethod
+    def run():
+        return 'Ingesting! Nom nom nom...'
